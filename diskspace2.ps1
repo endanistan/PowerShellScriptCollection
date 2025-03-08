6d46a60acebf08ef7c94c6f9340a215601632838
endanistan <daniel.berg1991@pm.me>
2025-03-08 22:45:20 +0100

    Add files via upload

--- diskspace2.ps1
+++ diskspace2.ps1
@@ -1,53 +1,69 @@
+#Script collects data about diskspace.
+#Use -free, -used or -max to only collect some data. No parameters collects all 3.
 param (
+
     [Switch]$free,
     [Switch]$used,
     [Switch]$max
+
 )
 
-$drives = Get-Volume | Where-Object {$_.Driveletter -ne $null}
+    #Collects all potential data.
+    $drives = Get-Volume | Where-Object {$_.Driveletter -ne $null} -ErrorAction Inquire
 
+#Loops data into array $everything based on set parameters.
 foreach ($volume in $drives) {
-    $everything = @()
-    $whole = $volume.Size
-    $part = $volume.SizeRemaining
-    $remaining = ($part / $whole * 100)
-    $usednr = ($volume.Size - $volume.SizeRemaining)
-    $usedp = ($usednr / $volume.Size * 100)
-    $wholegb = $volume.Size/1gb
+    #Ignores disks with a driveletters that are empty. Mainly to avoid potential issues with mounted dvd-drives.
+    if ($null -eq $volume.Size -or $volume.Size -eq 0) {
+        continue
+    }
 
-    if ($free) {
-        $everything += [PSCustomObject]@{
-            "Driveletter" = $volume.Driveletter
-            "Free drive capacity %" = $remaining
-        }
-            Write-Output $everything 
-    }
-    
-    if ($used) {
-        $everything += [PSCustomObject]@{
-            "Driveletter" = $volume.Driveletter
-            "Used drive capacity %" = $usedp
-        }
-            Write-Output $everything 
-        }  
-    
-    if ($max) {
-        $everything += [PSCustomObject]@{
-            "Driveletter" = $volume.Driveletter
-            "Drive max capacity" = $wholegb
-        }
-            Write-Output $everything 
-    }
-    
-    if (-not ($free -or $used -or $max)) {
+        $everything = @()
+        $whole = $volume.Size
+        $part = $volume.SizeRemaining
+        $remaining = ($part / $whole * 100)
+        $usednr = ($volume.Size - $volume.SizeRemaining)
+        $usedp = ($usednr / $volume.Size * 100)
+        $wholegb = $volume.Size/1gb
 
-        $everything += [PSCustomObject]@{
-            "Driveletter" = $volume.Driveletter
-            "Drive max capacity" = $wholegb
-            "Used drive capacity %" = $usedp
-            "Free drive capacity %" = $remaining
+            if ($free) {
+            
+                $everything += [PSCustomObject]@{
+                    "Driveletter" = $volume.Driveletter
+                    "Free drive capacity %" = $remaining
+                }
+
+            }
+    
+            if ($used) {
+            
+                $everything += [PSCustomObject]@{
+                    "Driveletter" = $volume.Driveletter
+                    "Used drive capacity %" = $usedp
+                }
+
+            }  
+    
+            if ($max) {
+            
+                $everything += [PSCustomObject]@{
+                    "Driveletter" = $volume.Driveletter
+                    "Drive max capacity" = $wholegb
+                }
+
+            }
+
+            if (-not ($free -or $used -or $max)) {
+
+                $everything += [PSCustomObject]@{
+                    "Driveletter" = $volume.Driveletter
+                    "Drive max capacity gb" = $wholegb
+                    "Used drive capacity %" = $usedp
+                    "Free drive capacity %" = $remaining
+                }
+
         }
-            Write-Output $everything 
-    }
+    
+    Write-Output $everything | Format-Table
                    
 }
