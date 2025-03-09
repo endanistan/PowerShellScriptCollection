fd4ce49b21fe2063b31f3f32a48da6399e913052
endanistan <daniel.berg1991@pm.me>
2025-03-09 11:48:02 +0100

    Add files via upload

---
+++ new_entra_user.ps1
@@ -0,0 +1,73 @@
+#Parameters for the script.
+#$UserPrincipalName expects f.ex. "UserName@domain.topleveldomain".
+param (
+    [Parameter(Mandatory = $true)][String]$DisplayName,
+    [Parameter(Mandatory = $true)][String]$UserPrincipalName,
+    [Parameter(Mandatory = $true)][String]$PWD,
+    [Parameter(Mandatory = $false)][String]$Group
+)  
+
+#Installs microsoft.graph if already not installed.
+try {
+    if  (-not (Get-Module -listAvailable -Name Microsoft.Graph)) {
+        Install-Module Microsoft.Graph -Scope CurrentUser -Repository PSGallery -Force
+        Write-Warning "Microsoft Graph module is not installed. Installing..."
+    }
+}
+catch {
+    Write-Output "Could not install Microsoft Graph."
+}
+
+#Connects user to microsoft.graph and if it fails tries again after changing ExecutionPolicy.
+for ($i = 1; $i -le 2; $i++) {
+    try {
+    Connect-MgGraph -scopes "user.readwrite.all, group.readwrite.all" -NoWelcome  
+    }
+    catch {
+    Write-Output "Could not connect to microsoft graph... trying to change execution policy..."
+        try {
+            Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+            Write-Output "ExecutionPolicy set to RemoteSigned. Trying to reconnect to microsoft graph."
+        }
+        catch {
+            Write-Output "ExecutionPolicy could not be changed, aborting script. Is pwsh.exe running as administrator?"
+        }
+    
+    }
+}
+
+#Adds new user to Microsoft Entra.
+#$MailNickname is "UserName" part of $UserPrincipalName see line 2 for reference.
+try {
+    $MailNickname = $UserPrincipalName.Split('@')[0]
+    $PWProfile = @{
+        Password = $PWD;
+        ForceChangePasswordNextSignIn = $false
+    }
+    
+    New-MgUser `
+        -DisplayName $DisplayName `
+        -MailNickname $MailNickname `
+        -UserPrincipalName $UserPrincipalName `
+        -PasswordProfile $PWProfile ` -AccountEnabled `
+}
+catch {
+    Read-Host "Unexpected error, $DisplayName not added to Microsoft Entra."
+}
+
+
+#Assigns the user to an existing group.
+#This script can not create new a MgGroup.
+if (-not $Group) {
+        $Group = Read-Host "User added without group assignment." -AsSecureString
+}
+else { try {
+        $AddToGroup = Get-MgGroup | Where-Object {$_.DisplayName -eq "$Group"}
+        $AddThisUser = Get-MgUser | Where-Object {$_.DisplayName -eq "$DisplayName"}
+        New-MgGroupMember -GroupId $AddToGroup.Id -DirectoryObjectId $AddThisUser.Id  
+    }
+    catch {
+        Read-Host "Unexpected error. User added, but without group assignment."
+    } 
+   
+}
