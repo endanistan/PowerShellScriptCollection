71e465501c0bff4c851b4d7beae556c5e9e7eab3
endanistan <daniel.berg1991@pm.me>
2025-03-09 11:48:12 +0100

    Add files via upload

---
+++ system_info.ps1
@@ -0,0 +1,10 @@
+$system = @()
+$system += [PSCustomObject]@{
+    "BIOS V." = (Get-CimInstance -Class Win32_BIOS).SMBIOSBIOSVersion
+    "Processor" = (Get-CimInstance -Class Win32_Processor).Name
+    "RAM manufacturer ID" = (Get-CimInstance -Class Win32_PhysicalMemory).PartNumber
+    "Disk model" = (Get-CimInstance -Class Win32_DiskDrive).Model
+    "Disk size" = (Get-CimInstance -Class Win32_DiskDrive).Size
+}
+
+Write-Output $system
