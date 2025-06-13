ed1ab0d1699dcec392a006779f92e19c6f94bfb4
endanistan <daniel.berg1991@outlook.com>
2025-06-13 21:37:35 +0200

    new_entra_user.ps1

--- new_entra_user.ps1
+++ new_entra_user.ps1
@@ -1,73 +1,42 @@
-#Parameters for the script.
-#$UserPrincipalName expects f.ex. "UserName@domain.topleveldomain".
+#$UserPrincipalName expects "UserName@domain.TopLevelDomain".
 param (
-    [Parameter(Mandatory = $true)][String]$DisplayName,
+    [Parameter(Mandatory = $true)][String]$tenantid,
+    [Parameter(Mandatory = $true)][String]$DisplayNameParameter,
     [Parameter(Mandatory = $true)][String]$UserPrincipalName,
     [Parameter(Mandatory = $true)][String]$PWD,
-    [Parameter(Mandatory = $false)][String]$Group
+    [Parameter(Mandatory = $false)][String[]]$Groups
+
 )  
 
-#Installs microsoft.graph if already not installed.
-try {
-    if  (-not (Get-Module -listAvailable -Name Microsoft.Graph)) {
-        Install-Module Microsoft.Graph -Scope CurrentUser -Repository PSGallery -Force
-        Write-Warning "Microsoft Graph module is not installed. Installing..."
-    }
-}
-catch {
-    Write-Warning "Could not install Microsoft Graph."
+if  (-not (Get-Module -listAvailable -Name Microsoft.Graph)) {
+    Install-Module Microsoft.Graph -Scope CurrentUser -Repository PSGallery -Force
 }
 
-#Connects user to microsoft.graph and if it fails tries again after changing ExecutionPolicy.
-for ($i = 1; $i -le 2; $i++) {
-    try {
-    Connect-MgGraph -scopes "user.readwrite.all, group.readwrite.all" -NoWelcome  
-    }
-    catch {
-    Write-Output "Could not connect to microsoft graph... trying to change execution policy..."
-        try {
-            Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-            Write-Output "ExecutionPolicy set to RemoteSigned. Trying to reconnect to microsoft graph."
-        }
-        catch {
-            Write-Warning "ExecutionPolicy could not be changed, aborting script. Is pwsh.exe running as administrator?"
-        }
-    
-    }
-}
+Connect-MgGraph -tenantid $tenantid -scopes "user.readwrite.all, group.readwrite.all" -NoWelcome  
 
 #Adds new user to Microsoft Entra.
 #$MailNickname is "UserName" part of $UserPrincipalName see line 2 for reference.
-try {
+    $GivenName = $DisplayNameParameter.Split(' ')[0]
+    $Surname = $DisplayNameParameter.Split(' ')[1..($DisplayNameParameter.Split(' ').Count - 1)] -join ' '
     $MailNickname = $UserPrincipalName.Split('@')[0]
-    $PWProfile = @{
-        Password = $PWD;
-        ForceChangePasswordNextSignIn = $false
-    }
+    $passwordprofile = @{}
+    $passwordprofile["Password"] = $PWD
+    $passwordprofile["forceChangePasswordNextSignIn"] = $True
+    $passwordprofile["forceChangePasswordNextSignInWithMfa"] = $False
     
     New-MgUser `
-        -DisplayName $DisplayName `
+        -DisplayName $DisplayNameParameter `
         -MailNickname $MailNickname `
         -UserPrincipalName $UserPrincipalName `
-        -PasswordProfile $PWProfile ` -AccountEnabled `
-}
-catch {
-    Read-Warning "Unexpected error, $DisplayName not added to Microsoft Entra."
-}
+        -GivenName $GivenName `
+        -Surname $Surname `
+        -PasswordProfile $Passwordprofile ` -AccountEnabled `
 
 
-#Assigns the user to an existing group.
-#This script can not create new a MgGroup.
-if (-not $Group) {
-        $Group = Read-Host "$DisplayName added without group assignment."
-}
-else { try {
-        $AddToGroup = Get-MgGroup | Where-Object {$_.DisplayName -eq "$Group"}
-        $AddThisUser = Get-MgUser | Where-Object {$_.DisplayName -eq "$DisplayName"}
-        New-MgGroupMember -GroupId $AddToGroup.Id -DirectoryObjectId $AddThisUser.Id  
+    if ($groups) {
+        foreach ($Group in $Groups) {
+        $AddToGroup = (Get-MgGroup | Where-Object {$_.DisplayName -eq "$Group"}).id
+        $AddThisUser = (Get-MgUser | Where-Object {$_.DisplayName -eq "$DisplayNameParameter"}).id
+        New-MgGroupMember -GroupId $AddToGroup -DirectoryObjectId $AddThisUser
+        }
     }
-    catch {
-        Read-Warning "Unexpected error. $DisplayName added, but without group assignment."
-    } 
-   
-}
