8934d867102829547ea6a22340fe726dd526c516
endanistan <daniel.berg1991@outlook.com>
2025-06-19 20:53:15 +0200

    Add files via upload

---
+++ Windows Client/edge_shortcut.ps1
@@ -0,0 +1,6 @@
+$edgeSource= "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+$desktopPath = "$ENV:ONEDRIVE\Desktop\Microsoft Edge.lnk"
+$WScriptObj = New-Object -ComObject ("WScript.Shell")
+$shortcut = $WScriptObj.CreateShortcut($desktopPath)
+$shortcut.TargetPath = $edgeSource
+$shortcut.Save()
