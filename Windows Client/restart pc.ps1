2f0b6f4adc494ba6bd7ca5284239c13e0492ffe9
endanistan <daniel.berg1991@outlook.com>
2025-06-19 20:53:40 +0200

    Add files via upload

---
+++ Windows Client/restart pc.ps1
@@ -0,0 +1,23 @@
+$log = "$ENV:TEMP\task_restart.log"
+test-path $log
+if ($log) {
+remove-item -path $log
+}
+
+Start-Transcript -path $log
+
+$upTime = (Get-Date) - (gcim Win32_OperatingSystem).LastBootUpTime
+if ($upTime.TotalHours -ge 36) {
+    $restartHour = Get-Date -Hour 3 -Minute 0 -Second 0
+    $correctRestartHour = $restartHour.AddDays(1)
+    $timeNow = Get-Date
+    $whenToRestart = $correctRestartHour - $timeNow
+    $inSeconds = [Math]::Round($whenToRestart.TotalSeconds)
+    shutdown /r /t $inSeconds
+    Write-Host "Restart was scheduled for $correctRestartHour"
+}
+else {
+    Write-Host "Restart not scheduled"
+}
+
+Stop-Transcript
