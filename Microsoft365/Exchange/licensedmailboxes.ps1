b15af3f5bae1e46460779cdd392fcb90c8106dac
endanistan <daniel.berg1991@outlook.com>
2025-07-02 06:00:07 +0200

    readwrite -> read

--- Microsoft365/Exchange/licensedmailboxes.ps1
+++ Microsoft365/Exchange/licensedmailboxes.ps1
@@ -1,5 +1,5 @@
 #Sort exchange mailboxes for sharedmailboxes only and download the csv file to a local path.
-connect-mggraph -tenantid "<tenant id>" -scope "User.ReadWrite.All" -nowelcome
+connect-mggraph -tenantid "<tenant id>" -scope "User.ReadW.All" -nowelcome
 
 $csvpath = "<path to csv file>"
 $ids = import-csv -path $csvpath | select-object -expandproperty objectid
