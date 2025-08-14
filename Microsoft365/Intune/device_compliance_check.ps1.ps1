4de43952992ddb186298914a18cd26b23fbdd3d7
endanistan <daniel.berg1991@outlook.com>
2025-08-14 23:35:28 +0200

    Add files via upload
    check last sign in of primary users of non-compliant devices

---
+++ Microsoft365/Intune/device_compliance_check.ps1.ps1
@@ -0,0 +1,14 @@
+﻿Connect-MgGraph -Tenantid "" -clientid "" -Scopes "DeviceManagementManagedDevices.Read.All", "Directory.Read.All", "User.Read.All", "Auditlog.Read.All" -NoWelcome
+$devices = Get-MgDeviceManagementManagedDevice -All | where-object {$_.Compliancestate -ne "compliant" } | Select-Object DeviceName, UserPrincipalName, OperatingSystem, OsVersion, LastSyncDateTime, UserId
+$results = foreach ($dev in $devices) {
+        $LastSignin = Get-MgUser -UserId $dev.UserId -Property "displayName,signInActivity" | Select-Object displayName,@{Name="LastSignInDate";Expression={$_.SignInActivity.LastSignInDateTime}}
+        [PSCustomObject]@{
+            Name         = $dev.DeviceName
+            UPN          = $dev.UserPrincipalName
+            OS           = "$($dev.OperatingSystem) $($dev.OsVersion)"
+            LastCheckIn  = $dev.LastSyncDateTime
+            LastSignin   = $LastSignin.LastSignInDate
+        }
+}
+ 
+$results | Sort-Object Name, UPN, OS, LastSignin, LastSyncDateTime | Export-Csv .\IntuneDevices.csv -Delimiter ";" -NoTypeInformation
