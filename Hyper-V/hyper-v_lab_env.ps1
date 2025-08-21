118156a971626e131dacca1db7f2fbfcac555830
endanistan <daniel.berg1991@outlook.com>
2025-08-21 20:27:34 +0200

    Update hyper-v_lab_env.ps1
    Some bugfixes and linting

--- Hyper-V/hyper-v_lab_env.ps1
+++ Hyper-V/hyper-v_lab_env.ps1
@@ -1,24 +1,23 @@
 Param (
-	[switch]$Create-Env,
-    [switch]$Create-Servers,
-    [switch]$Delete-Servers,
+	[switch]$CreateEnv,
+    [switch]$CreateServers,
+    [switch]$DeleteServers,
 	[Parameter(Mandatory = $false)][String]$Label
 )
 
 if (-not $Label) { $DiskLabel = "C" } else { $DiskLabel = $Label }
 $Servers = @("dc-01", "dc-02", "dhcp-01", "dhcp-02", "fs-01", "router-01")
-$VMLocation = "$DiskLabel:\Server Lab.Environment\Virtual Machines"
-$ISOLocation = "$DiskLabel:\Server Lab.Environment\ISO Files\en-us_windows_server_2025_updated_july_2025_x64_dvd_a1f0681d.iso"
-$VMStorageLocation = "$DiskLabel:\Server Lab.Environment\Virtual Machine Storage"
+$VMLocation = "${DiskLabel}:\Server Lab.Environment\Virtual Machines"
+$ISOLocation = "${DiskLabel}:\Server Lab.Environment\ISO Files\26100.1742.240906-0331.ge_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso"
+$VMStorageLocation = "${DiskLabel}:\Server Lab.Environment\Virtual Machine Storage"
 
 	function Create-Env {
-		New-Item -Path "$DiskLabel:\Server Lab.Environment" -ItemType Directory
-		New-Item -Path "$DiskLabel:\Server Lab.Environment\ISO Files" -ItemType Directory
-		New-Item -Path "$DiskLabel:\Server Lab.Environment\Server Service Scripts" -ItemType Directory
-		New-Item -Path "$DiskLabel:\Server Lab.Environment\Virtual Machines" -ItemType Directory
-		New-Item -Path "$DiskLabel:\Server Lab.Environment\Virtual Machine Storage" -ItemType Directory
-		Copy-Item -Path .\"en-us_windows_server_2025_updated_july_2025_x64_dvd_a1f0681d.iso" -Destination "$DiskLabel:\Server Lab.Environment\ISO Files"
-		foreach ($server in $servers) { New-Item -Path "$DiskLabel:\Server Lab.Environment\Virtual Machines\$server" -ItemType Directory }
+		New-Item -Path "${DiskLabel}:\Server Lab.Environment" -ItemType Directory
+		New-Item -Path "${DiskLabel}:\Server Lab.Environment\ISO Files" -ItemType Directory
+		New-Item -Path "${DiskLabel}:\Server Lab.Environment\Virtual Machines" -ItemType Directory
+		New-Item -Path "${DiskLabel}:\Server Lab.Environment\Virtual Machine Storage" -ItemType Directory
+		Copy-Item -Path .\"26100.1742.240906-0331.ge_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso" -Destination "${DiskLabel}:\Server Lab.Environment\ISO Files"
+		foreach ($server in $servers) { New-Item -Path "${DiskLabel}:\Server Lab.Environment\Virtual Machines\$server" -ItemType Directory }
 	}
 	
 	function Create-VMServers {
@@ -26,10 +25,12 @@ $VMStorageLocation = "$DiskLabel:\Server Lab.Environment\Virtual Machine Storage
 			New-VHD -Path "$VMStorageLocation\$server.vhdx" -Dynamic -SizeBytes 64GB
 			New-VM -name "$server" -memorystartupbytes 4GB -Generation 2 -Path "$VMLocation\$server"
 			Set-VM -name "$server" -ProcessorCount 8
-			Add-VMHardDiskDrive -VMName "server" -Path "$VMStorageLocation\$server.vhdx"
-			Add-VMDvdDrive -VMName "server" -Path "$ISOLocation"
+			Add-VMHardDiskDrive -VMName "$server" -Path "$VMStorageLocation\$server.vhdx"
+			Add-VMDvdDrive -VMName "$server" -Path "$ISOLocation"
 			Set-VMProcessor -VMName "$server" -ExposeVirtualizationExtensions $true
-			Set-VMFirmware -VMName "$server" -EnableSecureBoot On -FirstBootDevice $DVDDrive
+			$fw  = Get-VMFirmware -VMName $server
+			$DVDBoot = $fw.BootOrder | Where-Object { $_.FirmwarePath -like "*scsi(0,1)" }
+			Set-VMFirmware -VMName $server -EnableSecureBoot On -FirstBootDevice $DVDBoot
 			Add-VMNetworkAdapter -VMName "$server" -SwitchName "WLAN" | Set-VMNetworkAdapter -MacAddressSpoofing On
 			Set-VMKeyProtector -VMName "$server" -NewLocalKeyProtector
 			Enable-VMTPM -VMName "$server"
@@ -37,7 +38,7 @@ $VMStorageLocation = "$DiskLabel:\Server Lab.Environment\Virtual Machine Storage
 	}
 	
 	function Delete-VMServers {
-		foreach ($server in $servers)
+		foreach ($server in $servers) {
 			Stop-VM -VMName "$server" -TurnOff -Force
 			Get-VMHardDiskDrive -VMName "$server" -ControllerType SCSI | Remove-VMHardDiskDrive
 			$TestVHDPath = Test-Path -path "$VMStorageLocation\$server.vhdx" -ErrorAction SilentlyContinue
@@ -46,5 +47,14 @@ $VMStorageLocation = "$DiskLabel:\Server Lab.Environment\Virtual Machine Storage
 		}
 	}
 	
-if ($Create-Servers) { Create-VMServers } if ($Delete-Servers) { Delete-VMServers } else { Create-Env }
+if ($CreateServers) {
+Create-VMServers 
+}
 
+if ($DeleteServers) {
+Delete-VMServers 
+}
+
+if ($CreateEnv) {
+Create-Env 
+}
