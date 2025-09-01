b092f3fe7e4bad8b67267e8e31d6c917b167b498
endanistan <daniel.berg1991@outlook.com>
2025-09-01 21:55:58 +0200

    new_entra_user.ps1
    big overhaul of structure and added functionality for csv group list import

--- Microsoft365/Entra/new_entra_user.ps1
+++ Microsoft365/Entra/new_entra_user.ps1
@@ -1,7 +1,8 @@
 param (
     [Parameter(Mandatory = $false)][String]$DN,
     [Parameter(Mandatory = $false)][String]$UPN,
-    [Parameter(Mandatory = $false)][String[]]$Groups
+    [Parameter(Mandatory = $false)][String[]]$Groups,
+	[Parameter(Mandatory = $false)][String]$CSVList
 )  
 
     function UserPassword {
@@ -20,23 +21,20 @@ param (
 
     function NewEntraUser {
         $RandomPassword = UserPassword
-        $GivenName = $DN.Split(' ')[0]
-        $Surname = $DN.Split(' ')[1..($DN.Split(' ').Count - 1)] -join ' '
-        $MailNickname = $UPN.Split('@')[0]
-        $passwordprofile = @{}
-        $passwordprofile["Password"] = $RandomPassword
-        $passwordprofile["forceChangePasswordNextSignIn"] = $True
-        $passwordprofile["forceChangePasswordNextSignInWithMfa"] = $False
-    
+		$passwordprofile = @{
+			Password = $RandomPassword
+			forceChangePasswordNextSignIn = $true
+			forceChangePasswordNextSignInWithMfa = $false
+		}
         New-MgUser `
             -DisplayName $DN `
-            -MailNickname $MailNickname `
+            -MailNickname ($UPN.Split('@')[0]) `
             -UserPrincipalName $UPN `
-            -GivenName $GivenName `
-            -Surname $Surname `
+            -GivenName ($DN.Split(' ')[0]) `
+            -Surname ($DN.Split(' ')[1..($DN.Split(' ').Count - 1)] -join ' ') `
             -PasswordProfile $passwordprofile ` -AccountEnabled `
         
-        if ((Get-MgUser | Where-Object {$_.DisplayName -eq "$DN"}).id) {
+        if ((Get-MgUser -Filter "displayName eq '$DN'").Id) {
             Write-Host "$DN added with username $UPN and their password is copied to your clipboard" -ForegroundColor Green
         } else {
             Write-Host "User $DN could not be added"
@@ -44,57 +42,83 @@ param (
     }
     
     function UserGroups {
-        foreach ($Group in $Groups) {
-            $AddToGroup = (Get-MgGroup | Where-Object {$_.DisplayName -eq "$Group"}).id
-            $AddThisUser = (Get-MgUser | Where-Object {$_.DisplayName -eq "$DN"}).id
-            if (-not $AddToGroup) {
-                Write-Warning "The group $group could not be found, skipping..."
-                continue
-            } else {
-                New-MgGroupMember -GroupId $AddToGroup -DirectoryObjectId $AddThisUser -ErrorAction SilentlyContinue
-            }
-        }
-            foreach ($Group in $Groups) {
-                $CheckThisGroup = (Get-MgGroup | Where-Object {$_.DisplayName -eq "$Group"}).id
-                $IsUserInGroup = Get-MgGroupMemberAsUser -groupid $CheckThisGroup | Where-Object {$_.DisplayName -eq "$DN"}
-                if ($IsUserinGroup) {
-                    Write-Host "$DN have been added to the group $Group"  -ForegroundColor Green
-                } else {
-                    Write-Host "$DN has not been added to the group $Group" -ForegroundColor Yellow
-                }
-            }
-    }
+		param (
+		[switch]$CSV
+		)
+		if ($CSV) {
+			$GroupsFromCsv = Import-Csv -Path $CSVList -Delimiter ";" | Select-Object -ExpandProperty DisplayName
+			foreach ($Group in $GroupsFromCsv) {
+				$CSVAddToGroup = (Get-MgGroup -Filter "displayName eq '$Group'").Id
+				$CSVAddThisUser = (Get-MgUser -Filter "displayName eq '$DN'").Id
+				if (-not $CSVAddToGroup) {
+					Write-Warning "The group $Group could not be found, skipping..."
+					continue
+				} else {
+					Write-Host "Adding $DN to $Group" -Foreground Cyan
+					New-MgGroupMember -GroupId $CSVAddToGroup -DirectoryObjectId $CSVAddThisUser -ErrorAction SilentlyContinue
+				}
+			}
+		} else {		
+			foreach ($Group in $Groups) {
+				$AddToGroup = (Get-MgGroup | Where-Object {$_.DisplayName -eq "$Group"}).id
+				$AddThisUser = (Get-MgUser | Where-Object {$_.DisplayName -eq "$DN"}).id
+				if (-not $AddToGroup) {
+					Write-Warning "The group $Group could not be found, skipping..."
+					continue
+				} else {
+					Write-Host "Adding $DN to $Group" -Foreground Cyan
+					New-MgGroupMember -GroupId $AddToGroup -DirectoryObjectId $AddThisUser -ErrorAction SilentlyContinue
+				}
+			}
+		}
+	}
 
-if ($DN -or $UPN -or $Groups) { 
-    $TenantId = "" #Add tenantid
-    Connect-MgGraph -tenantid $TenantId -scopes "user.readwrite.all", "group.read.all", "RoleManagement.ReadWrite.Directory" -NoWelcome
-}
 
 if ($DN -and $UPN) {
+	$SecretValue = ""
+	#$ClientSecret = ""
+	$ClientID = ""
+	$TenantID = ""
+
+	$body = @{
+		grant_type    = "client_credentials"
+		scope         = "https://graph.microsoft.com/.default"
+		client_id     = $ClientID
+		client_secret = $SecretValue
+	}
+	$headers = @{ "Content-Type" = "application/x-www-form-urlencoded" }
+	$TokenResponse = Invoke-RestMethod `
+		-Uri "https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token" `
+		-Method Post `
+		-Body $body `
+		-Headers $headers
+	$AccessToken = $TokenResponse.access_token
+	$SecureToken = ConvertTo-SecureString $AccessToken -AsPlainText -Force
+	
+	Write-Host "Attempting to connect to Microsoft Graph..." -ForeGroundColor Magenta
+	Connect-MgGraph -AccessToken $SecureToken -NoWelcome
+	
     $CheckUPN = Get-MgUser -Filter "UserPrincipalName eq '$UPN'" -ErrorAction SilentlyContinue
     if ($CheckUPN) {
-        Write-Host "User $UPB already exists" -ForegroundColor Red
-        Break
+        Write-Host "User $UPN already exists, preceeding to add groups" -ForegroundColor Red
     } else {
         Write-Host "User $DN does not exist, proceeding to create user..." -ForegroundColor Cyan
         NewEntraUser
     }
-} else {
-    Write-Host "No user added, specify value for -DN and -UPN"
-}
+	
+	if ($null -eq $Groups -and $null -eq $CSVList) {
+		Write-Warning "No groups specified!"
+	}
+	if ($Groups) {
+		UserGroups
+	}
+	if ($CSVList) {
+		UserGroups -CSV
+	}
+} else { Write-Warning Specify DisplayName (DN), and UserPrincipalName (UPN)! }
 
-If ($Groups) {
-        $CheckUserName = Get-MgUser -Filter "DisplayName eq '$DN'" -ErrorAction SilentlyContinue
-        if (-not $CheckUserName) {
-            Write-Warning "User $DN does not exist. Could not add groups."
-            Break
-        } else {
-            Write-Host "User $DN exists, proceeding to add to groups..." -ForegroundColor Cyan
-            UserGroups
-        }
-} else {
-    Write-Warning "No groups specified."
-}
 
 Disconnect-MgGraph
-Write-Host "Script finished." -ForegroundColor Yellow
+Write-Host "Script finished. Disconnected Graph..." -ForegroundColor Magenta
+Start-Sleep -seconds 2
+Write-Host "Graph disconnected." -ForegroundColor Magenta
