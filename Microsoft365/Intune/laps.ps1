9255b6e385229b037fe829d587a6891b8a323b73
endanistan <daniel.berg1991@outlook.com>
2025-09-24 20:04:57 +0200

    Update laps.ps1

--- Microsoft365/Intune/laps.ps1
+++ Microsoft365/Intune/laps.ps1
@@ -4,31 +4,20 @@
 
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
-function GeneratePassword {
-    $letterNumberArray = @('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*')
-    for (($counter = 0); $counter -lt 20; $counter++) {
-        $randomCharacter = get-random -InputObject $letterNumberArray
-        $randomString = $randomString + $randomCharacter
-    }
-    return $randomString
-}
-
 $log = "$ENV:TEMP\laps.log"
 Start-Transcript -Path $log -Append
 
 Write-Host "Initiating laps script..."
 
-$Password = GeneratePassword
-$SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
-$UserName = "local-admin"
+$SecurePassword = (ConvertTo-SecureString -String "rN8zFT6O3C$^L*P09u" -AsPlainText -Force)
+$UserName = "LAPSAdmin"
 $AdminGroup = (Get-CimInstance -Class Win32_Group | Where-Object { $_.SID -eq "S-1-5-32-544" }).Name
 $User = Get-LocalUser -Name $UserName -ErrorAction SilentlyContinue
 
 if ($null -eq $User) {
     Write-Host "Creating local admin user: $UserName"
     New-LocalUser -Name $UserName -Password $SecurePassword -FullName "Laps Admin" -Description "LAPS-managed administrator"
-}
-else {
+} else {
     Write-Host "Local admin, $UserName, already exists."
 }
 
@@ -36,13 +25,12 @@ else {
 $AdminInGroup = Get-LocalGroupMember -Group $AdminGroup | Where-Object { $_.Name -like "*$UserName" } -ErrorAction SilentlyContinue
 if ($AdminInGroup) {
     Write-Host "Local admin, $UserName, already exists in the local administrators group." #This line should realistically never be reached.
-    Break
-}
-else {
+    break
+} else {
     $User2 = Get-LocalUser -Name $UserName -ErrorAction SilentlyContinue
     if ($User2) {
         Write-Host "Adding local admin, $UserName, to the local administrators group."
-        Add-LocalGroupMember -Group $AdminGroup -Member $UserName 
+        Add-LocalGroupMember -Group $AdminGroup -Member $UserName
     }
 }
 
@@ -50,8 +38,7 @@ else {
 $AdminInGroupExist = Get-LocalGroupMember -Group $AdminGroup | Where-Object { $_.Name -like "*$UserName" } -ErrorAction SilentlyContinue
 if ($AdminInGroupExist -like "*$UserName") {
     Write-Host "Local admin, $UserName, exists and is a member of the local administrators group."
-}
-else {
+} else {
     Write-Host "ERROR: Local admin, $UserName, is NOT part of the administrators group" #If this line is reached, start looking at line 24.
 }
 
