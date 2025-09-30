c1041bbc968c917aac5f274b0d6bf95eba97506e
endanistan <daniel.berg1991@outlook.com>
2025-09-30 11:46:08 +0200

    Update New-EntraUser.ps1

--- Entra ID & Intune/Create Entra User/New-EntraUser.ps1
+++ Entra ID & Intune/Create Entra User/New-EntraUser.ps1
@@ -6,7 +6,7 @@
 )
 
 function UserPassword {
-    $Adjectives = @("Ferocious", "Sabertoothed", "Maneating", "Bloodthirsty", "Vengeful", "Merciless", "Warthful", "Hellbound", "Soulharvesting", "Crazed", "Bleesed", "Flesheating")
+    $Adjectives = @("Ferocious", "Sabertoothed", "Maneating", "Bloodthirsty", "Vengeful", "Merciless", "Wrathful", "Hellbound", "Soulharvesting", "Crazed", "Blessed", "Flesheating")
     $Nouns = @("Goldfish", "Froglet", "Bumblebee", "Pig", "Capybara", "Toad", "Rabbit", "Lamb", "Crab", "Shrimp", "Starfish")
     $Numbers = @("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
     $Characters = @("#", "%", "!", "?", "+", "\", "*", "$", "/")
